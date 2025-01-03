69f00a827ea3ea1dd9d85ec64ab52e81178b9421
TimeCorroder <t1x3gg@gmail.com>
2025-01-03 11:13:11 +0800

    init

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+useHead
+  ({
+    title: 'My App',
+    meta: [
+      {
+        name: 'description',
+        content: 'My amazing site.'
+      }
+    ],
+    bodyAttrs: {
+      class: 'test'
+    },
+    script: [{}]
+  })
+</script>
